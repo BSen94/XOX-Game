2a4d702916913d2b974c19191e3db92cd3c58ab9
BSen94 <BSen94@users.noreply.github.com>
2021-04-03 15:45:14 +0300

    Initial commit

---
+++ app/src/main/java/com/bilgeadam/xox/activities/GameActivity.java
@@ -0,0 +1,58 @@
+package com.bilgeadam.xox.activities;
+
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.TextView;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentActivity;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
+import com.bilgeadam.xox.R;
+import com.bilgeadam.xox.fragments.GameBoard;
+import com.bilgeadam.xox.fragments.GameInfo;
+import com.bilgeadam.xox.game.Logic;
+
+public class GameActivity extends FragmentActivity {
+    public static final String GAME_KEY = "logic";
+
+    private Logic gameLogic;
+    private FragmentManager fragmentManager;
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        gameLogic = new Logic();
+
+        //Create bundle to deliver object to fragments
+
+        Bundle bundle = new Bundle();
+        bundle.putSerializable(GAME_KEY,gameLogic);
+
+        // Declare Game Info
+        Fragment gameInfo = new GameInfo();
+        gameInfo.setArguments(bundle);
+
+        Fragment gameBoard= new GameBoard();
+
+        //Initialize fragments via manager
+        fragmentManager = getSupportFragmentManager();
+        FragmentTransaction transaction = fragmentManager.beginTransaction();
+        transaction.add(R.id.game_info_frame, gameInfo);
+        transaction.add(R.id.game_board_frame, gameBoard);
+        transaction.commit();
+
+
+        setContentView(R.layout.activity_game);
+    }
+
+    public void clickImage(View view){
+        Log.i(this.getClass().getSimpleName(), String.format("Image tag: %s",view.getTag()));
+
+        int index= Integer.parseInt((String) view.getTag());
+
+        gameLogic.processTurn(index / 10 -1,index % 10 - 1);
+    }
+}
---
+++ app/src/main/java/com/bilgeadam/xox/animations/Animations.java
@@ -0,0 +1,61 @@
+package com.bilgeadam.xox.animations;
+
+import android.graphics.Rect;
+import android.view.View;
+import android.view.WindowManager;
+import org.jetbrains.annotations.NotNull;
+
+import java.util.Optional;
+
+public class Animations {
+
+    private final static long DURATION = 500;
+    private final static float ALPHA = 0.25F;
+
+    /**
+     * Rotates given text 360 degree for each 100ms.
+     * @param myText Text to be animated.
+     * @param duration Duration of the animation.
+     */
+
+    public void rotateText(@NotNull View myText, Optional<Long> duration){
+        long durationValue = duration.orElse(DURATION);
+        myText.animate().rotationXBy(360F * DURATION / 100).setDuration(DURATION).alpha(ALPHA);
+
+    }
+
+    /**
+     * Moves the button out of the screen
+     * @param myButton Button to be moved
+     * @param duration Duration of the animation
+     * @param currentWindow Current window manager
+     */
+
+    public void moveButton(@NotNull View myButton, Optional<Long> duration, WindowManager currentWindow){
+        long durationValue = duration.orElse(DURATION);
+
+        //Get current window boundaries
+        Rect bounds = currentWindow.getCurrentWindowMetrics().getBounds();
+
+        //Get current button location
+        int[] buttonLocation= new int[2];
+        myButton.getLocationInWindow(buttonLocation);
+
+        switch (Direction.generateRandomDirection()){
+            case Up:
+                myButton.animate().translationYBy(bounds.top-buttonLocation[1] - myButton.getHeight()).setDuration(durationValue).alpha(ALPHA);
+                break;
+            case Left:
+                myButton.animate().translationXBy(bounds.left-buttonLocation[0] - myButton.getWidth()).setDuration(durationValue).alpha(ALPHA);
+                break;
+            case Right:
+                myButton.animate().translationXBy(bounds.right-buttonLocation[0]).setDuration(durationValue).alpha(ALPHA);
+                break;
+            case Bottom:
+                myButton.animate().translationYBy(bounds.bottom-buttonLocation[1]).setDuration(durationValue).alpha(ALPHA);
+                break;
+        }
+
+    }
+
+}
